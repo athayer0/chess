7967aef40014d3856bafd0ba1f4d8e9550d86709
athayer0 <aaronsthayer@gmail.com>
2026-01-23 15:33:55 -0700

    implemented PieceMovesCalculator and bruteforced BishopMovesCalculator

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -1,12 +1,75 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class BishopMovesCalculator implements PieceMovesCalculator {
 
     @Override
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        List<ChessMove> moves = new ArrayList<>();
+
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        while (row < 8 && col < 8) {
+            ChessPosition endPosition = new ChessPosition(row+1, col+1);
+            if (board.getPiece(endPosition) != null) {
+                if (board.getPiece(endPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    moves.add(new ChessMove(myPosition, endPosition, null));
+                }
+                break;
+            }
+            moves.add(new ChessMove(myPosition, endPosition, null));
+            row++;
+            col++;
+        }
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+        while (row > 1 && col < 8) {
+            ChessPosition endPosition = new ChessPosition(row-1, col+1);
+            if (board.getPiece(endPosition) != null) {
+                if (board.getPiece(endPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    moves.add(new ChessMove(myPosition, endPosition, null));
+                }
+                break;
+            }
+            moves.add(new ChessMove(myPosition, endPosition, null));
+            row--;
+            col++;
+        }
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+        while (row > 1 && col > 1) {
+            ChessPosition endPosition = new ChessPosition(row-1, col-1);
+            if (board.getPiece(endPosition) != null) {
+                if (board.getPiece(endPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    moves.add(new ChessMove(myPosition, endPosition, null));
+                }
+                break;
+            }
+            moves.add(new ChessMove(myPosition, endPosition, null));
+            row--;
+            col--;
+        }
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+        while (row < 8 && col > 1) {
+            ChessPosition endPosition = new ChessPosition(row+1, col-1);
+            if (board.getPiece(endPosition) != null) {
+                if (board.getPiece(endPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    moves.add(new ChessMove(myPosition, endPosition, null));
+                }
+                break;
+            }
+            moves.add(new ChessMove(myPosition, endPosition, null));
+            row++;
+            col--;
+        }
+
+        return moves;
     }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -4,5 +4,5 @@ import java.util.Collection;
 
 public interface PieceMovesCalculator {
 
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
 }
