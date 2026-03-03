28d32ed0639abf5a2ddcf614fb597d128520d8da
athayer0 <aaronsthayer@gmail.com>
2026-03-02 21:56:21 -0700

    implemented clear endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
+import dataaccess.*;
 import io.javalin.*;
+import service.ClearService;
 
 public class Server {
 
@@ -11,6 +13,19 @@ public class Server {
 
         // Register your endpoints and exception handlers here.
 
+        // 1. Instantiate the Memory DAOs
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+
+        // 2. Instantiate the Service (injecting the DAOs)
+        ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
+
+        // 3. Instantiate the Handler (injecting the Service)
+        ClearHandler clearHandler = new ClearHandler(clearService);
+
+        // 4. Register the endpoint routes using Javalin
+        javalin.delete("/db", clearHandler::handleRequest);
     }
 
     public int run(int desiredPort) {
