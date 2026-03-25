5a5450d883ed87e81aa5fc0344c15a4136bfbd51
athayer0 <aaronsthayer@gmail.com>
2026-03-25 12:59:01 -0600

    added ServerFacade.java

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,72 @@
+package client;
+
+import com.google.gson.Gson;
+import model.AuthData;
+import model.UserData;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        this.serverUrl = url;
+    }
+
+    public AuthData register(UserData user) throws ResponseException {
+        var path = "/user";
+        return makeRequest("POST", path, user, AuthData.class, null);
+    }
+
+    public void clear() throws ResponseException {
+        var path = "/db";
+        makeRequest("DELETE", path, null, null, null);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String authToken) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            if (authToken != null) {
+                http.addRequestProperty("authorization", authToken);
+            }
+
+            if (request != null) {
+                http.addRequestProperty("Content-Type", "application/json");
+                String reqData = new Gson().toJson(request);
+                try (OutputStream reqBody = http.getOutputStream()) {
+                    reqBody.write(reqData.getBytes());
+                }
+            }
+
+            http.connect();
+
+            if (http.getResponseCode() >= 400) {
+                throw new ResponseException(http.getResponseCode(), "Server Error: " + http.getResponseCode());
+            }
+
+            return readBody(http, responseClass);
+
+        } catch (Exception ex) {
+            if (ex instanceof ResponseException) {
+                throw (ResponseException) ex;
+            }
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        if (responseClass == null) {
+            return null;
+        }
+
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader reader = new InputStreamReader(respBody);
+            return new Gson().fromJson(reader, responseClass);
+        }
+    }
+}
