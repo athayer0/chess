7c89fd3415b6ff4c51e8667f8b25b1e15a296d02
athayer0 <aaronsthayer@gmail.com>
2026-03-25 13:03:06 -0600

    register tests on ServerFacadeTests.java pass

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,27 @@
 package client;
 
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
+    }
+
+    @BeforeEach
+    public void clearData() throws ResponseException {
+        facade.clear();
     }
 
     @AfterAll
@@ -20,10 +29,25 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerSuccess() {
+        UserData user = new UserData("player1", "password", "p1@email.com");
+
+        assertDoesNotThrow(() -> {
+            AuthData auth = facade.register(user);
+            assertNotNull(auth);
+            assertNotNull(auth.authToken(), "Auth token should not be null after successful registration");
+            assertEquals("player1", auth.username(), "Username returned should match the requested username");
+        });
     }
 
+    @Test
+    public void registerDuplicateUser() throws ResponseException {
+        UserData user = new UserData("player1", "password", "p1@email.com");
+        facade.register(user);
+        ResponseException exception = assertThrows(ResponseException.class, () -> {
+            facade.register(user);
+        });
+        assertEquals(403, exception.statusCode(), "Expected a 403 status code for duplicate registration");
+    }
 }
