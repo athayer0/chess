7d8fd4633ae4f66ebdfb6ece032b2459f9989893
athayer0 <aaronsthayer@gmail.com>
2026-03-25 13:41:20 -0600

    impl Repl.java

---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,34 @@
+package client;
+
+import java.util.Scanner;
+
+public class Repl {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        this.client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("♕ Welcome to CS240 Chess. Type 'help' to get started. ♕");
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+
+        while (!result.equals("quit")) {
+            System.out.print("\n[LOGGED_OUT] >>> ");
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+
+                if (!result.equals("quit")) {
+                    System.out.println(result);
+                }
+            } catch (Throwable e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println("Thanks for playing!");
+    }
+}
